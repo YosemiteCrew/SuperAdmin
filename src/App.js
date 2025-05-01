ad4953bbb572cd339fbdca7df3c2114eee94f0fd
dunexploration-frontend <frontenddeveloper@dunexploration.com>
2025-05-01 13:26:05 +0530

    Feat: dashboard profile sec

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import { Route, Routes } from "react-router-dom";
 import AdminDashboard from "./Pages/AdminDashboard/page";
 import SignIn from "./Pages/SignIn/page";
-import ProfileDetails from "./Pages/ProfileDetails/page";
+import AdminProfile from "./Pages/AdminProfile/AdminProfile";
+import SettingProfile from "./Pages/AdminProfile/SettingProfile";
 
 
 
@@ -12,7 +13,8 @@ function App() {
       <Routes>
         <Route path="/" element={<AdminDashboard />} />
         <Route path="/signin" element={<SignIn />} />
-        <Route path="/profile" element={<ProfileDetails />} />
+        <Route path="/Adminprofile" element={<AdminProfile/>} />
+        <Route path="/settingprofile" element={<SettingProfile/>} />
       </Routes>
 
      
