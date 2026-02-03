23984ec67093e36c8aa4a459fbf9ee2a6587e7be
suryansh-yc <suryansh@yosemitecrew.com>
2026-02-03 16:43:44 +0530

    fix: Added new admin & removed old apps

--- apps/frontend/src/app/(routes)/assessments/page.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-import React from 'react'
-import AssessmentsPage from '@/app/Pages/ContentManagement/Assessments/AssessmentsPage'
-
-function page() {
-  return <AssessmentsPage />
-}
-
-export default page
